0c377d3f41020cc0af873708e978989ef2528b3e
Jaime Luis Osorio Gómez <jaosoriogo@unal.edu.co>
2024-02-25 22:02:48 -0500

    Subiendo Código

---
+++ televisores/control.py
@@ -0,0 +1,52 @@
+class Control:
+    
+    def _init_ (self, tv):
+        
+        self._tv = tv
+        
+    def getTv (self):
+        
+        return self._tv
+    
+    def turnOn(self):
+        
+        self._tv.turnOn()
+        
+    def turnOff (self):
+        
+        self._tv.turnOff()
+        
+    def canalUp (self):
+        
+        self._tv.canalUp()
+        
+    def canalDown (self):
+        
+        self._tv.canalDown()
+        
+    def volumenUp (self):
+        
+        self._tv.volumenUp()
+    
+    def volumenDown (self):
+        
+        self._tv.volumenDown()
+        
+    def setVolumen (self, volumen):
+        
+        self._tv.setVolumen(volumen)
+        
+    def setCanal (self, canal):
+        
+        self._tv.setCanal(canal)
+        
+    def enlazar (self, tv):
+        
+        self._tv = tv
+        self._tv.setControl(self)
+        
+        
+    
+    
+        
+    
